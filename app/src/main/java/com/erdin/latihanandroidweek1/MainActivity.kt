4f566ecd86d0a27ec3343ae279ff037906bff74e
erdinsuharyadi <erdin.sdzeko@gmail.com>
2020-08-12 10:31:19 +0700

    lifecycle + log

--- app/src/main/java/com/erdin/latihanandroidweek1/MainActivity.kt
+++ app/src/main/java/com/erdin/latihanandroidweek1/MainActivity.kt
@@ -3,7 +3,9 @@ package com.erdin.latihanandroidweek1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,18 +14,85 @@ class MainActivity : AppCompatActivity() {
 
         val btnActivityA = findViewById<Button>(R.id.btn_to_activity_a)
         val btnActivityB = findViewById<Button>(R.id.btn_to_activity_b)
-
-        val intentA = Intent(this, OneActivity::class.java)
-        val intentB = Intent(this, TwoActivity::class.java)
+        val btnActivityLL = findViewById<Button>(R.id.btn_to_linear_layout)
+        val btnActivityFL = findViewById<Button>(R.id.btn_to_frame_layout)
+        val btnActivityRL = findViewById<Button>(R.id.btn_to_relative_layout)
+        val btnActivityCL = findViewById<Button>(R.id.btn_to_constraint_layout)
 
         btnActivityA.setOnClickListener {
+            val intentA = Intent(this, OneActivity::class.java)
+            intentA.putExtra("domainName", "arkademy.com")
             startActivity(intentA)
         }
 
         btnActivityB.setOnClickListener {
+            val intentB = Intent(this, TwoActivity::class.java)
             startActivity(intentB)
         }
 
+        btnActivityLL.setOnClickListener {
+            val intent = Intent(this, LinearLayoutActivity::class.java)
+            startActivity(intent)
+        }
 
+        btnActivityFL.setOnClickListener {
+            val intent = Intent(this, FrameLayoutActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnActivityRL.setOnClickListener {
+            val intent = Intent(this, RelativeLayoutActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnActivityCL.setOnClickListener {
+            val intent = Intent(this, ConstraintLayoutActivity::class.java)
+            startActivity(intent)
+        }
+
+        Toast.makeText(this, "Activity: OnCreate()", Toast.LENGTH_LONG).show()
+        Log.d("lifecycle", "OnCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("lifecycle", "OnStart()")
+        Toast.makeText(this, "Activity: OnStart()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("lifecycle", "OnResume()")
+        Toast.makeText(this, "Activity: OnResume()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("lifecycle", "OnPause()")
+        Toast.makeText(this, "Activity: OnPause()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("lifecycle", "OnStop()")
+        Toast.makeText(this, "Activity: OnStop()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d("lifecycle", "OnRestart()")
+        Toast.makeText(this, "Activity: OnRestart()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("lifecycle", "onDestroy()")
+        Toast.makeText(this, "Activity: OnDestroy()", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/erdin/latihanandroidweek1/OneActivity.kt
+++ app/src/main/java/com/erdin/latihanandroidweek1/OneActivity.kt
@@ -1,11 +1,20 @@
 package com.erdin.latihanandroidweek1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class OneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one)
+
+        val tvDomain = findViewById<TextView>(R.id.tv_domain_name)
+
+        val domainName = intent.getStringExtra("domainName")
+        tvDomain.text = domainName
+        Log.d("domainString", domainName)
+
     }
 }
